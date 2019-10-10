9d9cfb3e21f5db5d1515e018b9f7af86d85e5b5e
ojustine <56227354+ojustine@users.noreply.github.com>
2019-10-10 16:10:45 +0300

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -12,11 +12,19 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 20
+# define BUFF_SIZE 512
+# define MALLCHECK(x) if (!(x)) return (-1);
 
+# include <limits.h>
 # include <stdlib.h>
 # include <string.h>
 
 int	get_next_line(const int fd, char **line);
 
+# if (BUFF_SIZE < 1)
+#  error "BUFF_SIZE must be strictly positive"
+# endif
+# if (BUFF_SIZE > SIZE_MAX)
+#  warning "BUFF_SIZE current value may cause a stack overflow"
+# endif
 #endif
